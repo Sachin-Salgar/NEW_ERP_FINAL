9718c74d0926ae4129bfc14423ea6d01d93fde16
Sachin-Salgar <sachinsalgar9@gmail.com>
2026-08-27 15:50:54 +0530

    fix: import ValueKey for web E2E compilation

--- frontend/integration_test/login_tenant_auth_e2e_test.dart
+++ frontend/integration_test/login_tenant_auth_e2e_test.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart' as http;
