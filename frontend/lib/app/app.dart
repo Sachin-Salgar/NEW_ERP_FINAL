acdfac3f85a5834d073aa8a1c8dd35a6feed47a0
Sachin-Salgar <sachinsalgar9@gmail.com>
2026-08-28 11:59:27 +0530

    feat(theme): connect app theme mode to persistent controller

--- frontend/lib/app/app.dart
+++ frontend/lib/app/app.dart
@@ -3,12 +3,13 @@ import 'package:get_it/get_it.dart';
 import 'package:provider/provider.dart';
 
 import '../core/auth/auth_service.dart';
-import '../core/network/api_client.dart';
 import '../modules/organization/organization_service.dart';
 import '../modules/branch/branch_service.dart';
 import '../modules/user/user_service.dart';
 import '../routing/router.dart';
 import '../themes/app_theme.dart';
+import '../themes/theme_controller.dart';
+import '../core/network/api_client.dart';
 
 final GetIt di = GetIt.instance;
 
@@ -17,6 +18,7 @@ class App extends StatefulWidget {
 
   static Future<void> init() async {
     di.registerLazySingleton<AuthService>(() => AuthService());
+    di.registerLazySingleton<ThemeController>(() => ThemeController());
     final baseUrl = const String.fromEnvironment(
       'API_BASE_URL',
       defaultValue: 'http://localhost:3000',
@@ -31,7 +33,11 @@ class App extends StatefulWidget {
     di.registerLazySingleton(() => UserService(apiClient: di.get<ApiClient>()));
 
     final auth = di.get<AuthService>();
-    await auth.init();
+    final theme = di.get<ThemeController>();
+    await Future.wait([
+      theme.init(),
+      auth.init(),
+    ]);
     await auth.bootstrap(baseUrl);
     if (auth.isAuthenticated) {
       await auth.restoreSession(baseUrl);
@@ -45,15 +51,20 @@ class App extends StatefulWidget {
 class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<AuthService>(
-      create: (_) => di.get<AuthService>(),
-      child: Consumer<AuthService>(
-        builder: (context, auth, _) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AuthService>.value(value: di.get<AuthService>()),
+        ChangeNotifierProvider<ThemeController>.value(value: di.get<ThemeController>()),
+      ],
+      child: Consumer2<AuthService, ThemeController>(
+        builder: (context, auth, themeController, _) {
           final route = auth.nextPostAuthRoute;
 
           return MaterialApp(
             title: 'NEW ERP',
             theme: AppTheme.lightTheme,
+            darkTheme: AppTheme.darkTheme,
+            themeMode: themeController.themeMode,
             onGenerateRoute: AppRouter.generateRoute,
             initialRoute: route,
           );
