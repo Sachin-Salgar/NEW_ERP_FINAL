e5db75ca94b8ef014d04b46fc8ac13ef0e7bb504
Sachin-Salgar <sachinsalgar9@gmail.com>
2026-09-22 13:51:03 +0530

    Fix Dart formatting in auth client

--- frontend/lib/core/network/api_client.dart
+++ frontend/lib/core/network/api_client.dart
@@ -77,7 +77,10 @@ class ApiClient {
 
   Future<http.Response> get(String path) async {
     final url = Uri.parse('$baseUrl$path');
-    return _sendWithAuth(path, (headers) => _client.get(url, headers: headers));
+    return _sendWithAuth(
+      path,
+      (headers) => _client.get(url, headers: headers),
+    );
   }
 
   Future<http.Response> put(String path, {Map<String, dynamic>? body}) async {
